96173e9fe7fadad507369d5e99afea33b3588424
gdi2290 <github@gdi2290.com>
2014-01-11 14:51:36 -0800

    add live reload and watch files for example page

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,11 @@
 'use strict';
 
+var LIVERELOAD_PORT = 35729;
+var lrSnippet = require('connect-livereload')({ port: LIVERELOAD_PORT });
+var mountFolder = function(connect, dir) {
+  return connect.static(require('path').resolve(dir));
+};
+
 // # Globbing
 // for performance reasons we're only matching one level down:
 // 'test/spec/{,*/}*.js'
@@ -57,9 +63,48 @@ module.exports = function(grunt) {
           '<%= bwr.name %>.js': ['./lib/index.js', './lib/*/*.js']
         }
       }
+    },
+    watch: {
+      livereload: {
+        options: {
+          livereload: LIVERELOAD_PORT
+        },
+        files: [
+          'example/{,*/}*.html',
+          'example/{,*/}*.js',
+          '{,*/}*.js'
+        ]
+      }
+    },
+    connect: {
+      options: {
+        port: '3000',
+        // Change this to '0.0.0.0' to access the server from outside.
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          middleware: function (connect) {
+            return [
+              lrSnippet,
+              mountFolder(connect, '.tmp'),
+              mountFolder(connect, 'example'),
+              mountFolder(connect, '.')
+            ];
+          }
+        }
+      }
+
     }
+
   });
 
+  grunt.registerTask('server', function (target) {
+    grunt.task.run([
+      'connect:livereload',
+      'watch'
+    ]);
+  });
 
   grunt.registerTask('test', [
   ]);
